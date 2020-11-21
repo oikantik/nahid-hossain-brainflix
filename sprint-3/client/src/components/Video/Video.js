d482d9eee3be6c4050333664b6279a52186f6f8a
Nahid Hossain <nahid@Nahids-MacBook-Pro.local>
2020-11-21 16:59:12 -0500

    play pause full screen volume mutes are added

--- sprint-3/client/src/components/Video/Video.js
+++ sprint-3/client/src/components/Video/Video.js
@@ -1,20 +1,102 @@
-import React from "react";
+import React, { Component, createRef } from "react";
 import "./Video.scss";
 import VideoControls from "../VideoControls/VideoControls";
 
-function Video({ posterImage, duration, videoUrl }) {
-  return (
-    <section className="video">
-      <div className="video-container">
-        <video
-          className="video-container__video"
-          src={videoUrl}
-          poster={posterImage}
-        ></video>
-        <VideoControls duration={duration} />
-      </div>
-    </section>
-  );
+class Video extends Component {
+  constructor() {
+    super();
+    this.videoRef = createRef();
+    this.videoParentRef = createRef();
+  }
+
+  state = {
+    play: false,
+    duration: 0,
+    progress: 0,
+    fullscreen: false,
+    muted: false,
+  };
+
+  playVideo = () => {
+    this.setState({ play: true }, () => {
+      this.videoRef.current.play();
+    });
+  };
+
+  getDuration = () => {
+    console.log(this.videoRef);
+    this.setState({
+      duration: Math.ceil(this.videoRef.current.duration),
+    });
+  };
+
+  pauseVideo = () => {
+    this.setState({ play: false }, () => {
+      this.videoRef.current.pause();
+    });
+  };
+
+  showFullScreen = () => {
+    if (!this.state.fullscreen) {
+      this.setState({ fullscreen: true }, () => {
+        this.videoParentRef.current.requestFullscreen();
+      });
+    }
+    if (this.state.fullscreen) {
+      document.exitFullscreen().then(() => {
+        this.setState({ fullscreen: false });
+      });
+    }
+  };
+
+  volumeClick = () => {
+    if (this.videoRef.current.muted) {
+      this.setState(
+        { muted: false },
+        () => (this.videoRef.current.muted = false)
+      );
+    }
+
+    if (!this.videoRef.current.muted) {
+      this.setState(
+        { muted: true },
+        () => (this.videoRef.current.muted = true)
+      );
+    }
+  };
+
+  render() {
+    const { posterImage, videoUrl } = this.props;
+    return (
+      <section className="video">
+        <div className="video-container">
+          <div
+            className="video-container__video-area"
+            ref={this.videoParentRef}
+          >
+            <video
+              ref={this.videoRef}
+              className="video-container__video"
+              src={videoUrl}
+              poster={posterImage}
+              type="video/mp4"
+              onLoadedMetadata={this.getDuration}
+              controls={false}
+            ></video>
+            <VideoControls
+              duration={this.state.duration}
+              playVideo={this.playVideo}
+              pauseVideo={this.pauseVideo}
+              play={this.state.play}
+              showFullScreen={this.showFullScreen}
+              volumeClick={this.volumeClick}
+              muted={this.state.muted}
+            />
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Video;
--- sprint-3/client/src/components/VideoControls/VideoControls.js
+++ sprint-3/client/src/components/VideoControls/VideoControls.js
@@ -1,40 +1,94 @@
 import React from "react";
 import "./VideoControls.scss";
-import playPauseControlIcon from "../../assets/icons/SVG/Icon-play.svg";
+import playControlIcon from "../../assets/icons/SVG/Icon-play.svg";
+import pauseControlIcon from "../../assets/icons/SVG/Icon-pause.svg";
 import fullScreenControlIcon from "../../assets/icons/SVG/Icon-fullscreen.svg";
 import volumeControlIcon from "../../assets/icons/SVG/Icon-volume.svg";
+import muteControlIcon from "../../assets/icons/SVG/Icon-muted.svg";
 
 function VideoControls(props) {
+  const handlePlay = () => {
+    props.playVideo();
+  };
+
+  const handlePause = () => {
+    props.pauseVideo();
+  };
+
+  const handleFullScreen = () => {
+    props.showFullScreen();
+  };
+
+  const handleVolume = () => {
+    props.volumeClick();
+  };
+
   return (
     <div className="video-controls">
-      <div className="video-controls__play-pause">
-        <img
-          src={playPauseControlIcon}
-          alt="playPause"
-          className="video-controls__play-pause-image"
-        />
-      </div>
+      {!props.play && (
+        <div className="video-controls__play-pause">
+          <img
+            src={playControlIcon}
+            alt="playPause"
+            className="video-controls__play-pause-image"
+            onClick={handlePlay}
+          />
+        </div>
+      )}
+
+      {props.play && (
+        <div className="video-controls__play-pause">
+          <img
+            src={pauseControlIcon}
+            alt="playPause"
+            className="video-controls__play-pause-image"
+            onClick={handlePause}
+          />
+          )
+        </div>
+      )}
       <div className="video-controls__progress">
         <progress className="video-controls__progress-line" value="0" min="0">
           <span className="video-controls__progress-bar"></span>
         </progress>
-        <span className="video-controls__length">0.00 / {props.duration}</span>
+        <span className="video-controls__length">
+          0:00 /{" "}
+          {props.duration < 60
+            ? 0 + ":" + props.duration
+            : Math.floor(props.duration / 60) +
+              ":" +
+              Math.ceil(props.duation % 60)}
+        </span>
       </div>
       <div className="video-controls__rest">
-        <span className="video-controls__full-screen">
+        <span
+          className="video-controls__full-screen"
+          onClick={handleFullScreen}
+        >
           <img
             src={fullScreenControlIcon}
             alt="fullScreen"
             className="video-controls__full-screen-image"
           />
         </span>
-        <span className="video-controls__volume">
-          <img
-            src={volumeControlIcon}
-            alt="volume"
-            className="video-controls__volume-image"
-          />
-        </span>
+        {!props.muted && (
+          <span className="video-controls__volume" onClick={handleVolume}>
+            <img
+              src={volumeControlIcon}
+              alt="volume"
+              className="video-controls__volume-image"
+            />
+          </span>
+        )}
+        {props.muted && (
+          <span className="video-controls__volume" onClick={handleVolume}>
+            <img
+              src={muteControlIcon}
+              alt="volume"
+              className="video-controls__volume-image"
+            />
+          </span>
+        )}
       </div>
     </div>
   );
